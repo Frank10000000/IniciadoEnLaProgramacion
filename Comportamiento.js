97d40e4573b2c3c7b205e1ef1e01ea4f2e7ffff3
Frank10000000 <86135552+Frank10000000@users.noreply.github.com>
2023-02-27 06:42:54 -0300

    Update Comportamiento.js

--- Comportamiento.js
+++ Comportamiento.js
@@ -1,3 +1,3 @@
 document.getElementById("telefono").onmouseover = function (){document.getElementById("contenido").innerHTML = "3635145874"}
-document.getElementById("direccion").onmouseover = function (){document.getElementById("contenido").innerHTML = "elLadoOscuro32@yahoo.com.ar"}
+document.getElementById("direccion").onmouseover = function (){document.getElementById("contenido").innerHTML = "Via Lactea/Sistema Solar/Marte - elLadoOscuro32@yahoo.com.ar"}
 document.getElementById("experiencia").onmouseover = function (){document.getElementById("contenido").innerHTML = "Ganaderia, Construcción, Maquinaria pesada y Instructor de esgrima"}
